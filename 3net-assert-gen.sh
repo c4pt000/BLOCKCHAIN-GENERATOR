b9c206d4ab886bfe6732943ba5230c837af10910
c4pt000 <46433702+c4pt000@users.noreply.github.com>
2021-05-19 09:46:48 -0400

    Create 3net-assert-gen.sh

---
+++ 3net-assert-gen.sh
@@ -0,0 +1,242 @@
+#!/bin/bash -e
+# This script is an experiment to clone litecoin into a 
+# brand new coin + blockchain.
+# The script will perform the following steps:
+# 1) create first a docker image with ubuntu ready to build and run the new coin daemon
+# 2) clone GenesisH0 and mine the genesis blocks of main, test and regtest networks in the container (this may take a lot of time)
+# 3) clone litecoin
+# 4) replace variables (keys, merkle tree hashes, timestamps..)
+# 5) build new coin
+# 6) run 4 docker nodes and connect to each other
+# 
+# By default the script uses the regtest network, which can mine blocks
+# instantly. If you wish to switch to the main network, simply change the 
+# CHAIN variable below
+
+
+# warning: change this to your own pubkey to get the genesis block mining reward
+
+
+#change to your phrase + public key
+
+# build public key with sha256.py and pubkey.py
+# edit these lines
+PHRASE="Your phrase"
+LITECOIN_PUB_KEY=000000000000000-SAME-PUBLICKEY
+GENESIS_REWARD_PUBKEY=000000000000000-SAME-PUBLICKEY
+
+
+GENESISHZERO_REPOS=https://github.com/c4pt000/GenesisH0
+
+# LEAVE EMPTY DISREGARD
+LITECOIN_MERKLE_HASH=
+LITECOIN_MAIN_GENESIS_HASH=
+LITECOIN_TEST_GENESIS_HASH=
+LITECOIN_REGTEST_GENESIS_HASH=
+
+COIN_NAME="COIN-KEY-ASSERT"
+COIN_NAME_LOWER=$(echo $COIN_NAME | tr '[:upper:]' '[:lower:]')
+COIN_NAME_UPPER=$(echo $COIN_NAME | tr '[:lower:]' '[:upper:]')
+COIN_UNIT_LOWER=$(echo $COIN_UNIT | tr '[:upper:]' '[:lower:]')
+
+DIRNAME=$(dirname $0)
+DOCKER_NETWORK="172.18.0"
+DOCKER_IMAGE_LABEL="newcoin-env"
+OSVERSION="$(uname -s)"
+
+
+
+docker_build_image()
+{
+    IMAGE=$(docker images -q $DOCKER_IMAGE_LABEL)
+    if [ -z $IMAGE ]; then
+        echo Building docker image
+        if [ ! -f $DOCKER_IMAGE_LABEL/Dockerfile ]; then
+            mkdir -p $DOCKER_IMAGE_LABEL
+            cat <<EOF > $DOCKER_IMAGE_LABEL/Dockerfile
+FROM fedora:28
+RUN yum install  git nano python2-pip -y
+RUN pip install construct==2.5.2 scrypt
+EOF
+        fi 
+        docker build --label $DOCKER_IMAGE_LABEL --tag $DOCKER_IMAGE_LABEL $DIRNAME/$DOCKER_IMAGE_LABEL/
+    else
+        echo Docker image already built
+    fi
+}
+
+docker_run_genesis()
+{
+    mkdir -p $DIRNAME/.ccache
+    docker run -v $DIRNAME/GenesisH0:/GenesisH0 $DOCKER_IMAGE_LABEL /bin/bash -c "$1"
+}
+
+docker_run()
+{
+    mkdir -p $DIRNAME/.ccache
+    docker run -v $DIRNAME/GenesisH0:/GenesisH0 -v $DIRNAME/.ccache:/root/.ccache -v $DIRNAME/$COIN_NAME_LOWER:/$COIN_NAME_LOWER $DOCKER_IMAGE_LABEL /bin/bash -c "$1"
+}
+
+docker_stop_nodes()
+{
+    echo "Stopping all docker nodes"
+    for id in $(docker ps -q -a  -f ancestor=$DOCKER_IMAGE_LABEL); do
+        docker stop $id
+	docker container prune -f
+    done
+}
+
+docker_remove_nodes()
+{
+    echo "Removing all docker nodes"
+    for id in $(docker ps -q -a  -f ancestor=$DOCKER_IMAGE_LABEL); do
+        docker rm $id
+    done
+}
+
+docker_create_network()
+{
+    echo "Creating docker network"
+    if ! docker network inspect newcoin &>/dev/null; then
+        docker network create --subnet=$DOCKER_NETWORK.0/16 newcoin
+    fi
+}
+
+docker_remove_network()
+{
+    echo "Removing docker network"
+    docker network rm newcoin
+}
+
+
+generate_genesis_block()
+{
+    if [ ! -d GenesisH0 ]; then
+        git clone $GENESISHZERO_REPOS
+        pushd GenesisH0
+    else
+        pushd GenesisH0
+        git pull
+    fi
+
+    if [ ! -f ${COIN_NAME}-main.txt ]; then
+        echo "Mining genesis block... this procedure can take many hours of cpu work.."
+        docker_run_genesis "python /GenesisH0/genesis.py -a scrypt -z \"$PHRASE\" -p $GENESIS_REWARD_PUBKEY 2>&1 | tee /GenesisH0/${COIN_NAME}-main.txt"
+    else
+        echo "Genesis block already mined.."
+        cat ${COIN_NAME}-main.txt
+    fi
+
+    if [ ! -f ${COIN_NAME}-test.txt ]; then
+        echo "Mining genesis block of test network... this procedure can take many hours of cpu work.."
+        docker_run_genesis "python /GenesisH0/genesis.py  -t 1486949366 -a scrypt -z \"$PHRASE\" -p $GENESIS_REWARD_PUBKEY 2>&1 | tee /GenesisH0/${COIN_NAME}-test.txt"
+    else
+        echo "Genesis block already mined.."
+        cat ${COIN_NAME}-test.txt
+    fi
+
+    if [ ! -f ${COIN_NAME}-regtest.txt ]; then
+        echo "Mining genesis block of regtest network... this procedure can take many hours of cpu work.."
+        docker_run_genesis "python /GenesisH0/genesis.py -t 1296688602 -b 0x207fffff -n 0 -a scrypt -z \"$PHRASE\" -p $GENESIS_REWARD_PUBKEY 2>&1 | tee /GenesisH0/${COIN_NAME}-regtest.txt"
+    else
+        echo "Genesis block already mined.."
+        cat ${COIN_NAME}-regtest.txt
+    fi
+
+    MAIN_PUB_KEY=$(cat ${COIN_NAME}-main.txt | grep "^pubkey:" | $SED 's/^pubkey: //')
+    MERKLE_HASH=$(cat ${COIN_NAME}-main.txt | grep "^merkle hash:" | $SED 's/^merkle hash: //')
+    TIMESTAMP=$(cat ${COIN_NAME}-main.txt | grep "^time:" | $SED 's/^time: //')
+    BITS=$(cat ${COIN_NAME}-main.txt | grep "^bits:" | $SED 's/^bits: //')
+
+    MAIN_NONCE=$(cat ${COIN_NAME}-main.txt | grep "^nonce:" | $SED 's/^nonce: //')
+    TEST_NONCE=$(cat ${COIN_NAME}-test.txt | grep "^nonce:" | $SED 's/^nonce: //')
+    REGTEST_NONCE=$(cat ${COIN_NAME}-regtest.txt | grep "^nonce:" | $SED 's/^nonce: //')
+
+    MAIN_GENESIS_HASH=$(cat ${COIN_NAME}-main.txt | grep "^genesis hash:" | $SED 's/^genesis hash: //')
+    TEST_GENESIS_HASH=$(cat ${COIN_NAME}-test.txt | grep "^genesis hash:" | $SED 's/^genesis hash: //')
+    REGTEST_GENESIS_HASH=$(cat ${COIN_NAME}-regtest.txt | grep "^genesis hash:" | $SED 's/^genesis hash: //')
+
+    popd
+}
+
+
+if [ $DIRNAME =  "." ]; then
+    DIRNAME=$PWD
+fi
+
+cd $DIRNAME
+
+# sanity check
+
+case $OSVERSION in
+    Linux*)
+        SED=sed
+    ;;
+    Darwin*)
+        SED=$(which gsed 2>/dev/null)
+        if [ $? -ne 0 ]; then
+            echo "please install gnu-sed with 'brew install gnu-sed'"
+            exit 1
+        fi
+        SED=gsed
+    ;;
+    *)
+        echo "This script only works on Linux and MacOS"
+        exit 1
+    ;;
+esac
+
+
+if ! which docker &>/dev/null; then
+    echo Please install docker first
+    exit 1
+fi
+
+if ! which git &>/dev/null; then
+    echo Please install git first
+    exit 1
+fi
+
+case $1 in
+    stop)
+        docker_stop_nodes
+    ;;
+    remove_nodes)
+        docker_stop_nodes
+        docker_remove_nodes
+    ;;
+    clean_up)
+        docker_stop_nodes
+        for i in $(seq 2 5); do
+           docker_run_node $i "rm -rf /$COIN_NAME_LOWER /root/.$COIN_NAME_LOWER" &>/dev/null
+        done
+        docker_remove_nodes
+        docker_remove_network
+        rm -rf $COIN_NAME_LOWER
+        if [ "$2" != "keep_genesis_block" ]; then
+            rm -f GenesisH0/${COIN_NAME}-*.txt
+        fi
+        for i in $(seq 2 5); do
+           rm -rf miner$i
+        done
+    ;;
+    start)
+        if [ -n "$(docker ps -q -f ancestor=$DOCKER_IMAGE_LABEL)" ]; then
+            echo "There are nodes running. Please stop them first with: $0 stop"
+            exit 1
+        fi
+        docker_build_image
+        generate_genesis_block
+
+
+    ;;
+    *)
+        cat <<EOF
+Usage: $0 (start|stop|remove_nodes|clean_up)
+ - start: bootstrap environment, build and run your new coin
+ - stop: simply stop the containers without removing them
+ - remove_nodes: remove the old docker container images. This will stop them first if necessary.
+ - clean_up: WARNING: this will stop and remove docker containers and network, source code, genesis block information and nodes data directory. (to start from scratch)
+EOF
+    ;;
+esac
